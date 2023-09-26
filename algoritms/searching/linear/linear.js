f55ddc3110127a572b85b3a661a04c92aa5d523d
SerhiyGuivan <serhiy.guivan@gmail.com>
2023-09-27 01:29:53 +0300

    Fix: Replaced for-in loop with the for loop to avoid return index in String type

--- algoritms/searching/linear/linear.js
+++ algoritms/searching/linear/linear.js
@@ -1,6 +1,8 @@
 function linearSearch(arr, target) {
-  for (let i in arr) {
-    if (arr[i] === target) return i
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
   }
-  return -1
+  return -1;
 }
+
+linearSearch([2,3,4,1,7], 1) // 3
