e72469a4dc07446749d46c2069c69a790d4ac019
SerhiyGuivan <serhiy.guivan@gmail.com>
2023-10-06 02:10:56 +0300

    Fix: Optimized shift method for SLL

--- data-structures/singly-linked-list/src/SLList.ts
+++ data-structures/singly-linked-list/src/SLList.ts
@@ -95,16 +95,12 @@ export default class SLList<T> {
     const removedNode= this._head;
 
     if (this._head === this._tail) {
-      const val = this._head.val;
-
       this._head = null;
       this._tail = null;
-      this._length--;
-
-      return val;
+    } else {
+      this._head = removedNode.next;
     }
 
-    this._head = removedNode.next;
     this._length--;
 
     return removedNode.val;
