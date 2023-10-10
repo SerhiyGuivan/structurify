fc1d117ac58d954f0938357b857999fc78531f7d
SerhiyGuivan <serhiy.guivan@gmail.com>
2023-10-10 04:06:56 +0300

    Feat: Added main index.ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+import SinglyLinkedList from './src/data-structures/linked-list/singly-linked-list';
+import DoubleLinkedList from './src/data-structures/linked-list/double-linked-list';
+import Queue from './src/data-structures/queue/queue';
+import Stack from './src/data-structures/stack/stack';
+
+export default {
+  SinglyLinkedList,
+  DoubleLinkedList,
+  Queue,
+  Stack,
+}
