29a60504695a076733a01f8f6b1b7bf2871986fd
SerhiyGuivan <serhiy.guivan@gmail.com>
2023-10-14 23:17:15 +0300

    Fix: Updated main ts

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import SinglyLinkedList from './data-structures/linked-list/singly-linked-list';
+import DoubleLinkedList from './data-structures/linked-list/double-linked-list';
+import Queue from './data-structures/queue/queue';
+import Stack from './data-structures/stack/stack';
+import BinarySearchTree from './data-structures/tree/binary-search-tree';
+import MaxBinaryHeap from './data-structures/heap/max-binary-heap';
+
+export default {
+  SinglyLinkedList,
+  DoubleLinkedList,
+  Queue,
+  Stack,
+  BinarySearchTree,
+  MaxBinaryHeap
+}
