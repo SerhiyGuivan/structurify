10260d7e0dac70b041b6d1b043da9abeb81e83dc
SerhiyGuivan <serhiy.guivan@gmail.com>
2023-10-19 02:28:00 +0300

    Feat: Updated SinglyLinkedList unit tests.

--- test/unit/data-structures/linked-list/singly-linked-list.test.ts
+++ test/unit/data-structures/linked-list/singly-linked-list.test.ts
@@ -7,158 +7,116 @@ describe('SinglyLinkedList', () => {
     list = new SinglyLinkedList<number>();
   });
 
-  describe('push', () => {
-    it('should add elements to the end of the list', () => {
-      list.push(1);
-      list.push(2);
-      expect(list.toArray()).toEqual([1, 2]);
-    });
+  it('should initialize an empty linked list', () => {
+    expect(list.getHeadNode).toBeNull();
+    expect(list.getTailNode).toBeNull();
+    expect(list.getLength).toBe(0);
   });
 
-  describe('pop', () => {
-    it('should remove and return the last element', () => {
-      list.push(1);
-      list.push(2);
-      expect(list.pop()).toBe(2);
-      expect(list.toArray()).toEqual([1]);
-    });
+  it('should add elements to the end of the list using push', () => {
+    list.push(1);
+    list.push(2);
 
-    it('should return undefined when the list is empty', () => {
-      expect(list.pop()).toBeUndefined();
-    });
+    expect(list.toArray()).toEqual([1, 2]);
   });
 
-  describe('shift', () => {
-    it('should remove and return the first element', () => {
-      list.push(1);
-      list.push(2);
-      expect(list.shift()).toBe(1);
-      expect(list.toArray()).toEqual([2]);
-    });
+  it('should remove elements from the end of the list using pop', () => {
+    list.push(1);
+    list.push(2);
+    const removed = list.pop();
 
-    it('should return undefined when the list is empty', () => {
-      expect(list.shift()).toBeUndefined();
-    });
+    expect(removed).toBe(2);
+    expect(list.toArray()).toEqual([1]);
   });
 
-  describe('unshift', () => {
-    it('should add elements to the beginning of the list', () => {
-      list.unshift(1);
-      list.unshift(2);
-      expect(list.toArray()).toEqual([2, 1]);
-    });
+  it('should remove the first element using shift', () => {
+    list.push(1);
+    list.push(2);
+    const removed = list.shift();
+
+    expect(removed).toBe(1);
+    expect(list.toArray()).toEqual([2]);
   });
 
-  describe('get', () => {
-    it('should retrieve the value at the specified index', () => {
-      list.push(1);
-      list.push(2);
-      list.push(3);
-      expect(list.get(1)).toBe(2);
-    });
+  it('should add elements to the beginning of the list using unshift', () => {
+    list.unshift(1);
+    list.unshift(2);
 
-    it('should return undefined for an out-of-bounds index', () => {
-      list.push(1);
-      expect(list.get(1)).toBeUndefined();
-    });
+    expect(list.toArray()).toEqual([2, 1]);
   });
 
-  describe('set', () => {
-    it('should update the value at the specified index', () => {
-      list.push(1);
-      list.push(2);
-      list.push(3);
-      list.set(1, 4);
-      expect(list.toArray()).toEqual([1, 4, 3]);
-    });
+  it('should get the element at a specific index', () => {
+    list.push(1);
+    list.push(2);
+    list.push(3);
 
-    it('should return false for an out-of-bounds index', () => {
-      list.push(1);
-      expect(list.set(1, 2)).toBe(false);
-    });
+    expect(list.get(1)).toBe(2);
   });
 
-  describe('insert', () => {
-    it('should insert a value at the specified index', () => {
-      list.push(1);
-      list.push(2);
-      list.insert(1, 3);
-      expect(list.toArray()).toEqual([1, 3, 2]);
-    });
+  it('should set the element at a specific index', () => {
+    list.push(1);
+    list.push(2);
+    list.push(3);
 
-    it('should return false for an out-of-bounds index', () => {
-      list.push(1);
-      expect(list.insert(2, 2)).toBe(false);
-    });
+    const result = list.set(1, 4);
+
+    expect(result).toBe(true);
+    expect(list.toArray()).toEqual([1, 4, 3]);
   });
 
-  describe('remove', () => {
-    it('should remove and return the value at the specified index', () => {
-      list.push(1);
-      list.push(2);
-      list.push(3);
-      expect(list.remove(1)).toBe(2);
-      expect(list.toArray()).toEqual([1, 3]);
-    });
+  it('should insert an element at a specific index', () => {
+    list.push(1);
+    list.push(2);
+    list.push(3);
 
-    it('should return undefined for an out-of-bounds index', () => {
-      list.push(1);
-      expect(list.remove(1)).toBeUndefined();
-    });
+    const result = list.insert(1, 4);
+
+    expect(result).toBe(true);
+    expect(list.toArray()).toEqual([1, 4, 2, 3]);
   });
 
-  describe('reverse', () => {
-    it('should reverse the order of nodes in the list', () => {
-      list.push(1);
-      list.push(2);
-      list.push(3);
-      list.reverse();
-      expect(list.toArray()).toEqual([3, 2, 1]);
-    });
+  it('should remove an element at a specific index', () => {
+    list.push(1);
+    list.push(2);
+    list.push(3);
 
-    it('should return an empty list as is', () => {
-      expect(list.reverse().toArray()).toEqual([]);
-    });
+    const removed = list.remove(1);
+
+    expect(removed).toBe(2);
+    expect(list.toArray()).toEqual([1, 3]);
   });
 
-  describe('rotate', () => {
-    it('should rotate the order of nodes in the list to the right', () => {
-      list.push(1);
-      list.push(2);
-      list.push(3);
-      list.push(4);
-      list.push(5);
-      list.rotate(2);
-      expect(list.toArray()).toEqual([3, 4, 5, 1, 2]);
-    });
+  it('should reverse the linked list', () => {
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    list.reverse();
 
-    it('should rotate the order of nodes in the list to the left', () => {
-      list.push(1);
-      list.push(2);
-      list.push(3);
-      list.push(4);
-      list.push(5);
-      list.rotate(-1);
-      expect(list.toArray()).toEqual([5, 1, 2, 3, 4]);
-    });
-
-    it('should return the same list for a rotation of 0', () => {
-      list.push(1);
-      list.push(2);
-      expect(list.rotate(0).toArray()).toEqual([1, 2]);
-    });
+    expect(list.toArray()).toEqual([3, 2, 1]);
   });
 
-  describe('toArray', () => {
-    it('should convert the linked list into an array', () => {
-      list.push(1);
-      list.push(2);
-      list.push(3);
-      expect(list.toArray()).toEqual([1, 2, 3]);
-    });
+  it('should rotate the linked list', () => {
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    list.push(4);
+    list.rotate(2);
 
-    it('should return an empty array for an empty list', () => {
-      expect(list.toArray()).toEqual([]);
-    });
+    expect(list.toArray()).toEqual([3, 4, 1, 2]);
+  });
+
+  it('should create a linked list from an array', () => {
+    const data = [1, 2, 3, 4];
+    list = SinglyLinkedList.fromArray(data);
+
+    expect(list.toArray()).toEqual(data);
+  });
+
+  it('should clear the linked list', () => {
+    list.push(1);
+    list.push(2);
+    list.clear();
+
+    expect(list.toArray()).toEqual([]);
   });
 });
